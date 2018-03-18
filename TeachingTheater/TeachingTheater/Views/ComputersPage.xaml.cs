fe41ec09e8264952545efbdd581ab55569ced171
Stian Cronje <scronje@live.com>
2018-03-18 00:57:05 -0500

    Squashed 'HCI/' content from commit 758e6bc
    git-subtree-dir: HCI
    git-subtree-split: 758e6bc928589eebbd2d7e4236d2cd54243880f5

---
+++ TeachingTheater/TeachingTheater/Views/ComputersPage.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+using TeachingTheater.Models;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TeachingTheater.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ComputersPage : ContentPage
+    {
+        ObservableCollection<Computer> StudentComputers;
+
+        public ComputersPage()
+        {
+            InitializeComponent();
+            //Items = new ObservableCollection<string>
+            //{
+            //    "HCI_Student_001",
+            //    "HCI_Student_002",
+            //    "HCI_Student_003",
+            //    "HCI_Student_004",
+            //    "HCI_Student_005",
+            //    "HCI_Student_006",
+            //    "HCI_Student_007",
+            //    "HCI_Student_008"
+            //};
+
+            StudentComputers = new ObservableCollection<Computer>
+            {
+                new Computer{Name = "Teacher Controls", Description="Teacher", DetailPage=typeof(TeacherDetailPage)},
+                new Computer{Name = "HCI_Student_001", Description="Student", DetailPage=typeof(StudentDetailPage)},
+                new Computer{Name = "HCI_Student_002", Description="Student", DetailPage=typeof(StudentDetailPage)},
+                new Computer{Name = "HCI_Student_003", Description="Student", DetailPage=typeof(StudentDetailPage)},
+                new Computer{Name = "HCI_Student_004", Description="Student", DetailPage=typeof(StudentDetailPage)},
+                new Computer{Name = "HCI_Student_005", Description="Student", DetailPage=typeof(StudentDetailPage)},
+                new Computer{Name = "HCI_Student_006", Description="Student", DetailPage=typeof(StudentDetailPage)},
+                new Computer{Name = "HCI_Student_007", Description="Student", DetailPage=typeof(StudentDetailPage)},
+                new Computer{Name = "HCI_Student_008", Description="Student", DetailPage=typeof(StudentDetailPage)},
+                new Computer{Name = "HCI_Student_009", Description="Student", DetailPage=typeof(StudentDetailPage)},
+                new Computer{Name = "HCI_Student_010", Description="Student", DetailPage=typeof(StudentDetailPage)}
+            };
+
+            MyListView.ItemsSource = StudentComputers;
+            //MyListView.BackgroundColor = Color.FromHex("#96d1ff");
+        }
+
+        void Handle_ItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            if (e.Item == null)
+                return;
+
+            Computer tapped = e.Item as Computer;
+            Page load = (Page)Activator.CreateInstance(tapped.DetailPage);
+            load.Title = tapped.Name;
+            MasterPage.Instance.SetDetailPage(load);
+
+            //Deselect Item
+            ((ListView)sender).SelectedItem = null;
+
+        }
+    }
+}
