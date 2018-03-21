b162a9bb3fad4535f04d16d5023990767d38b430
Stian Cronje <scronje@live.com>
2018-03-21 00:01:54 -0500

    Symptom assessment upload

---
+++ HumanComputerInteraction/SymptomAssessment/SymptomAssessment/SymptomAssessment.UWP/Controls/CustomSwitchRendererUWP.cs
@@ -0,0 +1,24 @@
+﻿using SymptomAssessment.Controls;
+using SymptomAssessment.UWP.Controls;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.UWP;
+
+[assembly: ExportRenderer(typeof(CustomSwitch), typeof(CustomSwitchRendererUWP))]
+namespace SymptomAssessment.UWP.Controls
+{
+    public class CustomSwitchRendererUWP : SwitchRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Switch> e)
+        {
+            base.OnElementChanged(e);
+
+            if (e.OldElement == null)
+            {
+                var customSwitch = e.NewElement as CustomSwitch;
+                
+                Control.OnContent = customSwitch.OnText;
+                Control.OffContent = customSwitch.OffText;
+            }
+        }
+    }
+}
---
+++ HumanComputerInteraction/SymptomAssessment/SymptomAssessment/SymptomAssessment/Controls/CustomSwitch.cs
@@ -0,0 +1,46 @@
+﻿using Xamarin.Forms;
+
+namespace SymptomAssessment.Controls
+{
+    public class CustomSwitch : Switch
+    {
+        public static readonly BindableProperty OnTextProperty = BindableProperty.Create(
+            propertyName: "OnText",
+            returnType: typeof(string),
+            declaringType: typeof(CustomSwitch),
+            defaultValue: "On",
+            defaultBindingMode: BindingMode.TwoWay,
+            propertyChanged: OnTextPropertyChanged);
+
+        private static void OnTextPropertyChanged(BindableObject bindable, object oldValue, object newValue)
+        {
+            var control = (CustomSwitch)bindable;
+            control.OnText = newValue.ToString();
+        }
+
+        public static readonly BindableProperty OffTextProperty = BindableProperty.Create(
+            propertyName: "OffText",
+            returnType: typeof(string),
+            declaringType: typeof(CustomSwitch),
+            defaultValue: "Off",
+            defaultBindingMode: BindingMode.TwoWay,
+            propertyChanged: OffTextPropertyChanged);
+
+        private static void OffTextPropertyChanged(BindableObject bindable, object oldValue, object newValue)
+        {
+            var control = (CustomSwitch)bindable;
+            control.OffText = newValue.ToString();
+        }
+
+        public string OnText
+        {
+            get { return (string)GetValue(OnTextProperty).ToString(); }
+            set { SetValue(OnTextProperty, value); }
+        }
+        public string OffText
+        {
+            get { return (string)GetValue(OffTextProperty).ToString(); }
+            set { SetValue(OffTextProperty, value); }
+        }
+    }
+}
