4d3b6b5d498bef34bf218e4a4a171c1abc960809
Stian Cronje <scronje@live.com>
2018-03-26 20:13:20 -0500

    car insurance project

---
+++ DatabaseManagementSystems/TermProject/CarInsuranceProject/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Data.SQLite;
+
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        const string dbLocation = @"data source=C:\Users\Stian\Dropbox\SHSU\DatabaseManagement\Project\ProjectDatabase.db";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public void clean()
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SQLiteConnection connection;
+            var connectionString = (dbLocation + ";Version=3;");
+            connection = new SQLiteConnection(connectionString);
+            connection.Open();
+            SQLiteCommand comm = new SQLiteCommand("select *from User where uID='" + textBox1.Text + "' and pw= '" + textBox2.Text + "'", connection);
+            SQLiteDataReader dr = comm.ExecuteReader();
+            if (dr.Read())
+            {
+
+                Form2 frm = new Form2();
+                frm.Show();
+                this.Hide();
+
+            }
+            else
+            {
+                MessageBox.Show("Username or Password is incorrect");
+                clean();
+
+            }
+            connection.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.MaximizeBox = false;
+
+        }
+
+
+    }
+}
---
+++ DatabaseManagementSystems/TermProject/CarInsuranceProject/WindowsFormsApplication1/Form5.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form5 : Form
+    {
+        public Form5()
+        {
+            InitializeComponent();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Form2 nw = new Form2();
+            nw.Show();
+            this.Hide();
+        }
+
+       
+    }
+}
